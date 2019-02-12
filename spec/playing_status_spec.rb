9dbc8d0f3b93e9f10fea8c4441b0054e9022c9c4
Steven Christenson <steven.g.christenson@gmail.com>
2019-02-12 11:33:00 +1100

    Post pairing session

--- spec/playing_status_spec.rb
+++ spec/playing_status_spec.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require 'spec_helper'
 require_relative '../playing_status'
 
@@ -9,16 +10,21 @@ RSpec.describe PlayingStatus do
   end
 
   describe '#start' do
-    it 'changes playing to true' do
-      # subject.start
-      expect(subject.playing).to eq(true)
-    end
+    # it 'changes playing to true' do
+    #   binding.pry
+    #   # subject.start
+    #   expect(subject.playing).to eq(true)
+    # end
   end
 
   describe '#stop' do
     it 'changes playing to false' do
-      # expect(subject.stop).to change(subject.playing).from(true)
-      expect(subject.stop).to eq(false)
+      expect { subject.stop }.to change { subject.playing }.from(true).to(false)
+      # expect { subject.stop }.to raise_error(StandardError)
+
+      subject.stop
+      expect(subject.playing).to eq(true)
+      # expect(subject.stop).to eq(false)
     end
   end
 end
