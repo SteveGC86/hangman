76871ce102b1f5ed143995c9ba791ffaf63b5ede
Steven Christenson <steven.g.christenson@gmail.com>
2019-02-19 09:54:00 +1100

    start of spec added

--- spec/spec/turn_spec.rb
+++ spec/spec/turn_spec.rb
@@ -1,6 +1,15 @@
 require 'spec_helper'
 require_relative '../turn'
+require_relative '../guess'
 
 RSpec.describe Turn do
+  subject { Guess.new }
+  let(:word) { "follow" }
+  let(:correct_letter) { "o" }
 
+  describe '#check' do
+    it 'checks if a word contains a letter' do
+      expect()
+    end
+  end
 end
