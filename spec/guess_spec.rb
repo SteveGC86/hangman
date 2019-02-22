c01c0111e9f0e0674d3e8b0aeec9d27539ebb724
Steven Christenson <steven.g.christenson@gmail.com>
2019-02-22 11:44:20 +1100

    Replaced turn_spec with guess_spec turn_spec was initially testing the functionality of guess_spec not turn. This commit corrects the title of the spec and focuses the logic.

--- spec/guess_spec.rb
+++ spec/guess_spec.rb
@@ -1,15 +1,14 @@
 require 'spec_helper'
-require_relative '../turn'
 require_relative '../guess'
 
-RSpec.describe Turn do
+RSpec.describe Guess do
   subject { Guess.new }
   let(:word) { "follow" }
   let(:correct_letter) { "o" }
 
-  describe '#check' do
+  describe '#letter_checker' do
     it 'checks if a word contains a letter' do
-      expect()
+      expect(subject.letter_checker(correct_letter, word)).to eq(true)
     end
   end
 end
